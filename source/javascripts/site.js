859c06b986b119d8dc019d58ef07ff5c769a3995
rinbo <robin.b@outlook.com>
2018-09-12 15:30:45 +0200

    Add title animation fade-in and make navbar background color appear on scroll down

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,19 @@
-// This is where it all goes :)
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout
+    let myNav = document.querySelector('nav');
+    /*let borjessons = document.getElementById('borjessons')
+    setTimeout( () => {
+        borjessons.classList.add('title-background')
+    },500)*/
+    document.addEventListener('scroll', (e) => { 
+        
+        if (window.scrollY >= 300 ) {
+            myNav.classList.add("nav-colored");
+            myNav.classList.remove("nav-transparent");
+        } 
+        else {
+            myNav.classList.add("nav-transparent");
+            myNav.classList.remove("nav-colored");
+        }
+    });
+});
