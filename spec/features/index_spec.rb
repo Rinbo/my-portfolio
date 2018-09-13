705f36e4988bef2b499a3a87dd1127934da05e95
rinbo <robin.b@outlook.com>
2018-09-13 18:29:35 +0200

    Add some extra unit tests in index_spec.rb

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,12 +1,43 @@
 describe 'Index Page', type: :feature do
-    it 'displays project list' do
-      visit '/'
-  
-      expect(page).to have_css('.projects')
-  
-      within '.projects' do
-        expect(page).to have_content 'My First Website'
-        expect(page).to have_content 'FizzBuzz'
-      end
+  it 'displays project list' do
+    visit '/'
+
+    expect(page).to have_css('.project-area')
+
+    within '.project-area' do
+      expect(page).to have_content 'BMI Calculator'
+      expect(page).to have_content 'FizzBuzz'
+      expect(page).to have_content 'My blog'
     end
   end
+
+  it 'displays an About section' do
+    visit '/'
+
+    expect(page).to have_css('#about')
+
+    within '#about' do
+      expect(page).to have_content 'Robin Börjesson'      
+    end
+  end
+
+  it 'displays a section with links to social media accounts' do
+    visit '/'
+
+    expect(page).to have_css('.social-links')
+
+    within '.social-links' do      
+      expect(page).to have_link('', :href => 'https://github.com/Rinbo')
+    end
+  end
+
+  it 'has a masthead wit borjessons in it' do
+    visit '/'
+
+    expect(page).to have_css('#top')
+
+    within '#top' do      
+      expect(page).to have_content("borjessons")
+    end
+  end
+end
