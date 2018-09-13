a9c1cf9c259f65f26461e8a6a8ee97efc531b669
rinbo <robin.b@outlook.com>
2018-09-13 11:12:21 +0200

    Add function to start title-animation upon clicking up arrow in navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout
+    
+    let title = document.getElementById('borjessons');
+    let anim = document.getElementById('anim-link') 
+    anim.addEventListener('click', (e) => {
+        e.preventDefault;
+        title.classList.remove('title');
+        void title.offsetWidth;
+        title.classList.add('title');
+    })
+
     let myNav = document.querySelector('nav');    
     document.addEventListener('scroll', (e) => { 
         
-        if (window.scrollY >= 300 ) {
+        if (window.scrollY >= 350 ) {
             myNav.classList.add("nav-colored");
             myNav.classList.remove("nav-transparent");
         } 
