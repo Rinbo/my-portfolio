a121e130f2434935e4703a47c361307cf8b665d2
rinbo <robin.b@outlook.com>
2018-10-24 09:46:43 +0200

    Add long_description, technologies and github link for several projects to projects.yml in order to display them in popup modal

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -23,21 +23,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });      
 });
 
-let modal = document.getElementById('myModal')
-let span = document.getElementsByClassName("close")[0];
-
-function openModal() {
-    modal = document.getElementById('myModal')
-    span = document.getElementsByClassName("close")[0];   
+function openModal(index) {
+    let modal = document.getElementById(`myModal_${index}`)
+    let span = document.getElementsByClassName("close")[index];   
     modal.style.display = "block";
     span.onclick = function() {
         modal.style.display = "none";
     }
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
 
